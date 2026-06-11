437182f87103b26a7c67b17ef719b27aba3ae36e
andre <andrlzpt@protonmail.com>
2026-06-11 19:44:48 -0300

    :sparkles: Simple generator with tests

---
+++ internal/markov/generator_test.go
@@ -0,0 +1,37 @@
+package markov
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGenerate(t *testing.T) {
+	testcases := []struct {
+		name             string
+		input            []string
+		word             string
+		maxNumberOfWords int
+		want             []string
+	}{
+		{
+			name:             "deve gerar sequência correta",
+			input:            []string{"o", "rei", "foi", "embora"},
+			word:             "o",
+			maxNumberOfWords: 3,
+			want:             []string{"o", "rei", "foi"},
+		},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			chain := NewChain()
+			chain.Train(testcase.input)
+
+			result := Generate(chain, testcase.word, testcase.maxNumberOfWords)
+
+			if !reflect.DeepEqual(result, testcase.want) {
+				t.Fatalf("Generate() result = %#v, want %#v", result, testcase.want)
+			}
+		})
+	}
+}
