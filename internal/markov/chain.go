7263c95be21388fa080609295ec5cfe0b07cf8ee
andre <andrlzpt@protonmail.com>
2026-06-13 19:57:42 -0300

    :sparkles: Random word selection

--- internal/markov/chain.go
+++ internal/markov/chain.go
@@ -1,5 +1,7 @@
 package markov
 
+import "math/rand"
+
 type Chain struct {
 	Next map[string][]string
 }
@@ -27,4 +29,14 @@ func (c *Chain) NextWord(word string) string {
 	}
 
 	return options[0]
+}
+
+func (c *Chain) NextWordAtRandom(word string) string {
+	options := c.Next[word]
+
+	if len(options) == 0 {
+		return ""
+	}
+
+	return options[rand.Intn(len(options))]
 }
--- internal/markov/chain_test.go
+++ internal/markov/chain_test.go
@@ -2,6 +2,7 @@ package markov
 
 import (
 	"reflect"
+	"slices"
 	"testing"
 )
 
@@ -61,6 +62,35 @@ func TestNextWord(t *testing.T) {
 			result := chain.NextWord(testcase.next)
 			if result != testcase.want {
 				t.Fatalf("NextWord(%v) result = %#v, want = %#v", testcase.next, result, testcase.want)
+			}
+		})
+	}
+}
+
+func TestNextWordAtRandom(t *testing.T) {
+	testcases := []struct {
+		name  string
+		input []string
+		next  string
+		want  []string
+	}{
+		{
+			name:  "deve retornar uma das opcoes válidas",
+			input: []string{"o", "rei", "olhou", "o", "espelho"},
+			next:  "o",
+			want:  []string{"rei", "espelho"},
+		},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			chain := NewChain()
+			chain.Train(testcase.input)
+
+			result := chain.NextWordAtRandom(testcase.next)
+
+			if !slices.Contains(testcase.want, result) {
+				t.Fatalf("NextWordAtRandom() result = %#v, want = %#v", result, testcase.want)
 			}
 		})
 	}
