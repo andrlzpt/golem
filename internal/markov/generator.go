3d6293d1280a04f04fc85a9267d378912086ce22
andre <andrlzpt@protonmail.com>
2026-06-15 20:16:47 -0300

    :sparkles: Generate with bigram option

--- internal/markov/generator.go
+++ internal/markov/generator.go
@@ -1,5 +1,7 @@
 package markov
 
+import "strings"
+
 type next func(string) string
 
 func GenerateNextDumb(chain Chain, word string, maxNumberOfWords int) []string {
@@ -10,9 +12,23 @@ func GenerateNextAtRandom(chain Chain, word string, maxNumberOfWords int) []stri
 	return generateWith(word, maxNumberOfWords, chain.NextWordAtRandom)
 }
 
-func generateWith(word string, maxNumberOfWords int, n next) []string {
+func generateWith(input string, maxNumberOfWords int, n next) []string {
+	fields := strings.Fields(input)
+
+	if len(fields) == 1 {
+		return unigramGenerate(input, maxNumberOfWords, n)
+	}
+
+	if len(fields) == 2 {
+		return bigramGenerate(input, maxNumberOfWords, n)
+	}
+
+	return []string{}
+}
+
+func unigramGenerate(input string, maxNumberOfWords int, n next) []string {
 	result := []string{}
-	current := word
+	current := input
 	for range maxNumberOfWords {
 		if current == "" {
 			return result
@@ -21,5 +37,29 @@ func generateWith(word string, maxNumberOfWords int, n next) []string {
 		result = append(result, current)
 		current = n(current)
 	}
+	return result
+}
+
+func bigramGenerate(input string, maxNumberOfWords int, n next) []string {
+	parts := strings.Fields(input)
+	result := []string{parts[0], parts[1]}
+
+	first := parts[0]
+	second := parts[1]
+
+	for len(result) < maxNumberOfWords {
+		key := first + " " + second
+		nextWord := n(key)
+
+		if nextWord == "" {
+			return result
+		}
+
+		result = append(result, nextWord)
+
+		first = second
+		second = nextWord
+	}
+
 	return result
 }
